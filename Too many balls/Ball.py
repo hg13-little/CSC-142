8b7938e722ff7634dba5db9de8b5223fc4438bea
hector <hector.gonzalez001@albright.edu>
2026-02-17 11:43:38 -0500

    final

--- Too many balls/Ball.py
+++ Too many balls/Ball.py
@@ -14,9 +14,10 @@ class Ball():
         self.windowWidth = windowWidth
         self.windowHeight = windowHeight
 
-        self.image = pygame.image.load('images/ball.png')
+        self.image = pygame.image.load('ball.png')
         # A rect is made up of [x, y, width, height]
         self.ballRect = self.image.get_rect()
+        self.ballRect = self.rect 
         self.width = self.ballRect.width
         self.height = self.ballRect.height
         self.maxWidth = windowWidth - self.width
@@ -37,10 +38,56 @@ class Ball():
         if (self.x < -self.width) or (self.x >= self.windowWidth):
             self.xSpeed = -self.xSpeed
 
+
         # Update the Ball's x and y, using the speed in two directions
         self.x = self.x + self.xSpeed
+import pygame
+import random
+from math import sin
+
+class Ball:
+
+    def __init__(self, window, windowWidth, windowHeight):
+        self.window = window
+        self.windowWidth = windowWidth
+        self.windowHeight = windowHeight
+
+        self.image = pygame.image.load('ball.png')
+
+        # CREATE RECT (this fixes your error)
+        self.rect = self.image.get_rect()
+        self.ballRect = self.rect  # alias for older code
+
+        self.width = self.rect.width
+        self.height = self.rect.height
+        self.maxWidth = windowWidth - self.width
+        self.maxHeight = windowHeight - self.height
+
+        # Random start
+        self.x = random.randrange(0, self.maxWidth)
+        self.y = self.height
+
+        speedsList = [-7, -6, -5, -4, -3, 3, 4, 5, 6, 7]
+        self.xSpeed = random.choice(speedsList)
+        self.ySpeed = random.randrange(self.maxHeight, self.windowHeight * 2)
+
+        # Sync rect position
+        self.rect.topleft = (self.x, self.y)
+
+    def update(self):
+        # Bounce off side walls
+        if self.x < -self.width or self.x >= self.windowWidth:
+            self.xSpeed *= -1
+
+        # Move in arc
+        self.x += self.xSpeed
         self.y = self.windowWidth - self.ySpeed * sin(3.14 * self.x / self.maxWidth)
-        self.ballRect.x = self.x
+
+        # ALWAYS sync rect
+        self.rect.topleft = (self.x, self.y)
+
+    def draw(self):
+        self.window.blit(self.image, self.rect)
         self.ballRect.y = self.y
 
     def draw(self):
